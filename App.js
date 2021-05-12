f5421729d5f9c9c6593bc8f55333eb686cbc1369
SIVASAKTHI S <38631048+nanbasakthi@users.noreply.github.com>
2021-05-12 21:45:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './Component/MainDiv';
+import MainDiv from './Component/MainDiv';
+
+function App() {
+  return (
+    <div className="App">
+     <MainDiv/>
+    </div>
+  );
+}
+
+export default App;
