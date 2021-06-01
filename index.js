48bcd93a696013f0cdea5e79018857a09eb04449
SIVASAKTHI S <38631048+nanbasakthi@users.noreply.github.com>
2021-06-01 05:45:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import App from './App';  
+import Footer from './Component/Footer';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <Footer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+ 
